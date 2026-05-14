b518fcb4d6f1baf6a4046baf793373da1d177a45
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-05-14 17:41:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,13 @@
-const CACHE_NAME = 'hmad-v25';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192x192.png',
-  './icon-512x512.png'
-];
+const CACHE_NAME = 'hmad-v26';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => new Response('Offline', { status: 503 }))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    )).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
